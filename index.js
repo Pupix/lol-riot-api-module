3f9c4abfd491ded8baac20fe0ff1e894240d6814
Administrator <admin@example.com>
2017-04-29 23:02:57 +0200

    txpo

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*jslint browser: true, devel: true, node: true, ass: true, nomen: true, unparam: true, indent: 4 */
 
-(function () {
-    "use strict";
+(function() {
+  "use strict";
 
-    module.exports = require('./lib/index');
+  module.exports = require('./lib/index');
 
 }());
